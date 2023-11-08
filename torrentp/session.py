a6bca6b403d3a99ee6a4a5ee25d861fed7456a61
Harshal <148691869+illuminati-Dev@users.noreply.github.com>
2023-11-08 12:44:06 +0530

    Update session.py

--- torrentp/session.py
+++ torrentp/session.py
@@ -1,20 +1,28 @@
+import libtorrent as lt
+
 class Session:
     def __init__(self, libtorrent):
         self._user_agent = 'python client v0.1'
         self._listen_interfaces = '0.0.0.0'
-        self._port = '6881'
+        self._port = 6881  # Port should be an integer, not a string
         self._download_rate_limit = 0
-        self._download_rate_limit = 0
+        self._upload_rate_limit = 0  # Fixed variable name
         self._lt = libtorrent
 
     def create_session(self):
-        return self._lt.session({'listen_interfaces':f'{self._listen_interfaces}:{self._port}'})
-        
+        settings = {
+            'listen_interfaces': f'{self._listen_interfaces}:{self._port}',
+            'user_agent': self._user_agent,
+            'download_rate_limit': self._download_rate_limit,
+            'upload_rate_limit': self._upload_rate_limit
+        }
+        return self._lt.session(settings)
+
     def __str__(self):
-        pass
+        return f"Session(user_agent='{self._user_agent}', listen_interfaces='{self._listen_interfaces}:{self._port}')"
 
     def __repr__(self):
-        pass
+        return self.__str__()
 
     def __call__(self):
         return self.create_session()
