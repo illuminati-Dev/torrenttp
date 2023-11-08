b4548c56467d69a4c6579af719e66a6cbc75f66b
Harshal <148691869+illuminati-Dev@users.noreply.github.com>
2023-11-08 12:43:36 +0530

    Update torrent_downloader.py

--- torrentp/torrent_downloader.py
+++ torrentp/torrent_downloader.py
@@ -3,7 +3,6 @@ from .torrent_info import TorrentInfo
 from .downloader import Downloader
 import libtorrent as lt
 
-
 class TorrentDownloader:
     def __init__(self, file_path, save_path):
         self._file_path = file_path
@@ -19,21 +18,22 @@ class TorrentDownloader:
         if self._file_path.startswith('magnet:'):
             self._add_torrent_params = self._lt.parse_magnet_uri(self._file_path)
             self._add_torrent_params.save_path = self._save_path
-            self._downloader = Downloader(session=self._session(), torrent_info=self._add_torrent_params,
+            self._downloader = Downloader(session=self._session, torrent_info=self._add_torrent_params,
                                           save_path=self._save_path, libtorrent=lt, is_magnet=True)
         else:
             self._torrent_info = TorrentInfo(self._file_path, self._lt)
-            self._downloader = Downloader(session=self._session(), torrent_info=self._torrent_info(),
+            self._downloader = Downloader(session=self._session, torrent_info=self._torrent_info,
                                           save_path=self._save_path, libtorrent=None, is_magnet=False)
 
         self._file = self._downloader
         return self._file.download()
 
     def __str__(self):
-        pass
+        return f"TorrentDownloader(file_path={self._file_path}, save_path={self._save_path})"
 
     def __repr__(self):
-        pass
+        return self.__str__()
 
     def __call__(self):
-        pass
+        # You can define what you want the instance to do when called here.
+        pass  # Add your logic for the __call__ method if needed
