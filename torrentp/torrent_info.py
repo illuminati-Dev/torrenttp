4e975f7cb74960cf82b3520efa4992a32b0ffe9c
Harshal <148691869+illuminati-Dev@users.noreply.github.com>
2023-11-15 22:46:22 +0530

    Update torrent_info.py

--- torrentp/torrent_info.py
+++ torrentp/torrent_info.py
@@ -1,23 +1,21 @@
-import libtorrent as lt
-
 class TorrentInfo:
     def __init__(self, path, libtorrent):
         self._path = path
         self._lt = libtorrent
-        self._info = self.create_torrent_info()
+        self._info = self._lt.torrent_info(self._path)
 
     def show_info(self):
-        # You can implement a method to display information about the torrent here.
         pass
 
     def create_torrent_info(self):
-        return self._lt.torrent_info(self._path)
+        self._info = self._lt.torrent_info(self._path)
+        return self._info
 
     def __str__(self):
-        return f"TorrentInfo(path='{self._path}')"
+        pass
 
     def __repr__(self):
-        return self.__str__()
-
+        pass
+    
     def __call__(self):
         return self.create_torrent_info()
