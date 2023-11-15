a7478b0aca87832803c5beba5ea2d6e3e8041432
Harshal <148691869+illuminati-Dev@users.noreply.github.com>
2023-11-15 22:45:29 +0530

    Update downloader.py

--- torrentp/downloader.py
+++ torrentp/downloader.py
@@ -1,6 +1,6 @@
 import sys
 import time
-import logging
+
 
 class Downloader:
     def __init__(self, session, torrent_info, save_path, libtorrent, is_magnet):
@@ -8,48 +8,48 @@ class Downloader:
         self._torrent_info = torrent_info
         self._save_path = save_path
         self._file = None
+        self._status = None
+        self._name = ''
+        self._state = ''
         self._lt = libtorrent
         self._add_torrent_params = None
         self._is_magnet = is_magnet
 
     def status(self):
         if not self._is_magnet:
-            self._file = self._session.add_torrent({'ti': self._torrent_info, 'save_path': self._save_path})
+            self._file = self._session.add_torrent({'ti': self._torrent_info, 'save_path': f'{self._save_path}'})
+            self._status = self._file.status()
         else:
             self._add_torrent_params = self._torrent_info
             self._add_torrent_params.save_path = self._save_path
             self._file = self._session.add_torrent(self._add_torrent_params)
-        return self._file.status()
-
-    def download(self):
-        try:
-            logging.info(f'Start downloading {self.name}')
-            while not self._file.status().is_seeding:
-                s = self.status()
-
-                progress_message = '\r%.2f%% complete (down: %.1f kB/s up: %.1f kB/s peers: %d) %s' % (
-                    s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000,
-                    s.num_peers, s.state)
-
-                logging.info(progress_message)
-                sys.stdout.flush()
-                time.sleep(1)
-
-            logging.info(f'Successfully Downloaded {self.name}')
-            return self.name
-        except Exception as e:
-            logging.error(f"Error downloading {self.name}: {e}")
-            raise
+            self._status = self._file.status()
+        return self._status
 
     @property
     def name(self):
-        return self._file.status().name
+        self._name = self.status().name
+        return self._name
+
+    def download(self):
+        print(f'Start downloading {self.name}')
+        while not self._status.is_seeding:
+            s = self.status()
+
+            print('\r%.2f%% complete (down: %.1f kB/s up: %.1f kB/s peers: %d) %s' % (
+                s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000,
+                s.num_peers, s.state), end=' ')
+
+            sys.stdout.flush()
+            time.sleep(1)
+        print(f'Successfully Downloaded {self._status.name}')
+        return self._status.name
 
     def __str__(self):
-        return f"Downloader(session={self._session}, save_path='{self._save_path}')"
+        pass
 
     def __repr__(self):
-        return self.__str__()
+        pass
 
     def __call__(self):
-        pass  # Add your logic for the __call__ method if needed
+        pass
